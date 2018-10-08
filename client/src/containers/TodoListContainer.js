2b06676d2ee92bf5495a95846254af7fd5a6be43
Do Trung Kien <trungkien.keio@gmail.com>
2018-10-08 13:38:43 +0700

    update components

--- client/src/containers/TodoListContainer.js
+++ client/src/containers/TodoListContainer.js
@@ -1,25 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import Todo from "../components/Todo";
 import { connect } from "react-redux";
 import { deleteTodo, updateTodo } from "../actions";
 import { List } from "@material-ui/core";
 
-class TodoListContainer extends Component {
-  render() {
-    const todoList = this.props.todos.length
-      ? this.props.todos.map((todo, index) => (
+const TodoListContainer = ({ todos, deleteTodo, updateTodo }) => (
+  <List>
+    {todos.length
+      ? todos.map((todo, index) => (
           <Todo
             key={index}
             id={index}
             todo={todo}
-            deleteTodo={() => this.props.deleteTodo(index)}
-            updateTodo={() => this.props.updateTodo(index)}
+            deleteTodo={() => deleteTodo(index)}
+            updateTodo={() => updateTodo(index)}
           />
         ))
-      : null;
-    return <List>{todoList}</List>;
-  }
-}
+      : null}
+  </List>
+);
 
 const mapDispatchToProps = {
   deleteTodo,
