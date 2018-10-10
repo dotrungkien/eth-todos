655a9df4b84f2f0e4f5d8ccf143ca45f7240a920
Do Trung Kien <trungkien.keio@gmail.com>
2018-10-10 17:05:21 +0700

    refactor

--- client/src/containers/AddTodoContainer.js
+++ client/src/containers/AddTodoContainer.js
@@ -1,27 +1,27 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import AddTodo from "../components/AddTodo";
-import { addTodo } from "../actions";
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import AddTodo from '../components/AddTodo'
+import { addTodo } from '../actions'
 
 class AddTodoContainer extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       todoText: null
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleChange = e => {
-    if (e.target.value !== "undefined")
-      this.setState({ todoText: e.target.value });
-  };
+    if (e.target.value !== 'undefined')
+      this.setState({ todoText: e.target.value })
+  }
 
   handleSubmit = e => {
-    e.preventDefault();
-    this.props.addTodo(this.state.todoText);
-  };
+    e.preventDefault()
+    this.props.addTodo(this.state.todoText)
+  }
 
   render() {
     return (
@@ -29,15 +29,15 @@ class AddTodoContainer extends Component {
         handleChange={this.handleChange}
         handleSubmit={this.handleSubmit}
       />
-    );
+    )
   }
 }
 
 const mapDispatchToProps = {
   addTodo
-};
+}
 
 export default connect(
   null,
   mapDispatchToProps
-)(AddTodoContainer);
+)(AddTodoContainer)
