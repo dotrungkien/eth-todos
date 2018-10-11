0ca2d19663868eda744623d2ec559fb446d00dd8
Do Trung Kien <trungkien.keio@gmail.com>
2018-10-11 10:25:33 +0700

    refactor

--- client/src/utils/getWeb3.js
+++ client/src/utils/getWeb3.js
@@ -1,17 +1,17 @@
-import Web3 from "web3";
+import Web3 from 'web3'
 
 const getWeb3 = new Promise(resolve => {
-  window.addEventListener("load", () => {
-    var web3 = window.web3;
-    if (typeof web3 !== "undefined") {
-      web3 = new Web3(web3.currentProvider);
-      console.log("Injected web3 detected.");
+  window.addEventListener('load', () => {
+    var web3 = window.web3
+    if (typeof web3 !== 'undefined') {
+      web3 = new Web3(web3.currentProvider)
+      console.log('Injected web3 detected.')
     } else {
-      web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"));
-      console.log("No web3 instance injected, using local web3");
+      web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:9545'))
+      console.log('No web3 instance injected, using local web3')
     }
-    resolve({ web3 });
-  });
-});
+    resolve({ web3 })
+  })
+})
 
-export default getWeb3;
+export default getWeb3
